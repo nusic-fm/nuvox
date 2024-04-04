6859bfebfff7f5160bf28447a9f56db625157e7d
logesh2496 <logesh.r24@gmail.com>
2024-04-04 21:58:25 +0530

    fix: remove viem for build errors

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-// import { createConfig, http, Config } from "wagmi";
-// import { polygonMumbai } from "wagmi/chains";
+import { createConfig, http, Config } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
 import { LensClient, development } from "@lens-protocol/client";
 
 export interface IStorageProvider {
@@ -28,12 +28,12 @@ export const lensClient = new LensClient({
   environment: development,
   storage: new LocalStorageProvider(),
 });
-// export const config = (createConfig as any)({
-//   chains: [polygonMumbai],
-//   transports: {
-//     [polygonMumbai.id]: http(),
-//   },
-// });
+export const config = (createConfig as any)({
+  chains: [polygonMumbai],
+  transports: {
+    [polygonMumbai.id]: http(),
+  },
+});
 
 // declare module "wagmi" {
 //   interface Register {
